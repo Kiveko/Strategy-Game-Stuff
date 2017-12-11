0087f39aec0881b39fb1a8d2521002cf5890ed71
Kiveko <34457352+Kiveko@users.noreply.github.com>
2017-12-11 20:56:04 +0000

    Update FSBCore.java

--- Data/FSBCore.java
+++ Data/FSBCore.java
@@ -1,10 +1,19 @@
-
+//Kiveko
+//Import random number and import functionality
+import java.util.Random;
 import java.util.Scanner;
 
 public class FSBCore
 {
     public static void main(String[] args)
     {
-        //Variables
+        //Variables(System)
+        int loopCounter;
+        string userName, systemDebug;
+        
+        //Variables (Gameplay)
+        float attackPower,defensePower,Evasion;
+        int resourceFood,resourceStone,resourceTimber,resourceSilver;
         
     }
+}
