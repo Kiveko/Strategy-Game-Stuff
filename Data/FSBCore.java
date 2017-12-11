43e2b16e2c9ad05b51772446dad7462ac7427255
Kiveko <34457352+Kiveko@users.noreply.github.com>
2017-12-11 20:43:28 +0000

    Update FSBCore.java

--- Data/FSBCore.java
+++ Data/FSBCore.java
@@ -1,10 +1,4 @@
-/**
- * Fantasy Simulated Battle
- *
- * @author (Jacob Grayson)
- * @version (0.01)
- */
- 
+
 import java.util.Scanner;
 
 public class FSBCore
@@ -12,5 +6,5 @@ public class FSBCore
     public static void main(String[] args)
     {
         //Variables
-
+        
     }
