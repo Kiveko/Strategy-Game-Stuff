b2fd0616293528b73469c260e989dfb7c1bb21a2
Kiveko <34457352+Kiveko@users.noreply.github.com>
2017-12-11 20:36:30 +0000

    Update FSBCore.java

--- Data/FSBCore.java
+++ Data/FSBCore.java
@@ -12,19 +12,5 @@ public class FSBCore
     public static void main(String[] args)
     {
         //Variables
-        Scanner ScannerInput = new Scanner(System.in);
-        
-        int TroopCountA,TroopCountB;
-        String RaceSelection;
-        
-        System.out.println("Hello User! Welcome to fantasy battle simulator!");
-        System.out.println("S,H,B?");
-        
-        RaceSelection = ScannerInput.next();
-        if (RaceSelection == "S")
-        {
-            System.out.println("You selected Skeletons! Is this correct?");
-        }
-            
-        }
+
     }
